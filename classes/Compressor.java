c91e8d98668a6c4d73e68a8fa69a90238df0abe4
chrisyng <cyng@NN123-HA-15.HS.sd203.org>
2015-04-22 12:22:59 -0500

    more changes

---
+++ classes/Compressor.java
@@ -0,0 +1,81 @@
+public class Compressor
+{
+    private SimplePicture uncompressed;
+    private Pixel[][] uncompressedPixels;
+    public Compressor(SimplePicture picture)
+    {
+        uncompressed = picture;
+        uncompressedPixels = picture.getPixels2D();
+    }
+
+    public String getDimensions()
+    /**
+     * @return the dimensions of the picture.     
+     */
+    {
+        return uncompressedPixels.length + " " + uncompressedPixels[0].length;
+    }
+
+    public String compress()
+    /**
+     * Run the compression algorithm: go row-by-row, find matching pixels with RGB values within 5 of each other, and store it as a String of integers in the order red, green, blue, and length of the block.
+     * @return string with compressed data
+     */
+    {
+        String compressedData = "";
+        for (int i=0; i< uncompressedPixels.length; i++)
+        {
+            Pixel previousPixel;
+            int numMatching=0; //stores amount of similar pixels in a row
+            for (int j=0; j<uncompressedPixels[0].length; j++)
+            {
+                previousPixel=uncompressedPixels[i][j-1];
+                if (checkSameColor(previousPixel, uncompressedPixels[i][j]))
+                {
+                    numMatching++; // check if the pixels are the "same" and increment the block size
+                }
+                else // calculate the average RGB values and send that to the compressed String of data
+                {
+
+                }
+            }
+        }
+    }
+    
+    public int[] calculateAverageRGB(Pixel[] block)
+    /**
+     * Helper method that finds the average of a given block of RGB values
+     * @param block the block of pixels that needs to have its average color calculated
+     * @return the average RGB values of a given "pixel block" as a 3-integer array
+     */
+    {
+        int totalRed=0;
+        int totalGreen=0;
+        int totalBlue = 0;
+        int blockLength = block.length;
+        for (Pixel pixel: block)
+        {
+            totalRed += pixel.getRed();
+            totalGreen += pixel.getGreen();
+            totalBlue += pixel.getBlue();
+        }
+        return new int[]{totalRed/blockLength, totalGreen/blockLength, totalBlue/blockLength};
+    }
+
+    public boolean checkSameColor(Pixel first, Pixel second)
+    /**
+     * Helper method that checks for similarity between pixels.
+     * @param first first pixel for comparison
+     * @param second second pixel for comparison
+     */
+    {
+        if (Math.abs(first.getRed()-second.getRed())< 5 &&
+        Math.abs(first.getGreen()-second.getGreen())<5 &&
+        Math.abs(first.getBlue()-second.getBlue())<5
+        )
+        {
+            return true;
+        }
+        return false;
+    }
+}
